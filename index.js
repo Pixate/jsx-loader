266118279409a63e2465cb806dae537725bf47ea
Justin Belcher <justin.belcher@gmail.com>
2014-03-14 22:46:53 -0400

    Surface JSX parse errors

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var React = require('react-tools');
 
 module.exports = function(source) {
-  this.cacheable();
-  return React.transform(source);
+  try {
+    this.cacheable();
+    return React.transform(source);
+  } catch (e) {
+    console.error('[%s]: ', this.resourcePath.split('/').pop(), e.message);
+    return source;
+  }
 };
